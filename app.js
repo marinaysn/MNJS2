d47057cb40fd5ff6489f315eed551a7add402c8c
Marina <marinaysn@gmail.com>
2019-10-04 00:18:12 -0400

    parsing routs

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 // to start run 'node .\app.js' or 'npm start'
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
-app.use('/', (req, res, next)=>{
-    console.log('This always runs');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+// app.use('/', (req, res, next)=>{
+//     //console.log('This always runs');
+//     next();
     
-})
+// })
 
 app.use('/addProduct',(req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>Add Products</h1>');
+   // console.log('In another middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next)=>{
+    console.log("*******************")
+    console.log(req.body)
+    res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
-    console.log('In another middleware')
+  //  console.log('In another middleware')
     res.send('<h1>Hello from Express</h1>');
 });
 
