c59b242bd8afeed63ca44c46182c325f940c611b
Marina <marinaysn@gmail.com>
2019-10-07 01:08:32 -0400

    added pug to addProduct as well

--- routes/admin.js
+++ routes/admin.js
@@ -6,20 +6,19 @@ const rootDir = require('../util/path');
 const products = [];
 
 router.get('/addProduct',(req, res, next) => {
-    // console.log('In another middleware')
-     res.sendFile(path.join(rootDir, 'views', 'addProduct.html'));
+
+    // res.sendFile(path.join(rootDir, 'views', 'addProduct.html'));
+
+     res.render('addProduct', {docTitle: 'Add Product'});
+
  });
  
  router.post('/addProduct', (req, res, next)=>{
-    //  console.log("1*******************");
-    //  console.log(req.body);
+
      products.push({title: req.body.title});
-    //  console.log("2*******************");
-    //  console.log(products[0].title);
-    //  console.log(products);
      res.redirect(301, '/');
  });
 
-//  module.exports = router;
+
 exports.routes = router;
 exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
 const adminData = require("./admin")
 const routes = express.Router()
 
 
 routes.get('/', (req, res, next) => {
-    console.log("3*******************");
-    console.log(adminData.products)
-
     const products = adminData.products;
-
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     res.render('shop', {prods: products, docTitle: 'My Shopping List'});
 
 });
