5017f47098f90a86d3385cb6229fce7a06d4826d
Marina <marinaysn@gmail.com>
2019-10-08 21:26:06 -0400

    clean up code

--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,7 @@ const routes = express.Router()
 routes.get('/', (req, res, next) => {
     const products = adminData.products;
 
-    // //use this fofr PUG template (uncomment)
-    //res.render('shop', {prods: products, docTitle: 'My Shopping List', path: '/shop', hasProducts: products.length > 0});
-
-    // //use this for HANDLEBARS template (uncomment)
+    // //use this for HANDLEBARS and EJS template (uncomment)
     res.render('shop', {prods: products, docTitle: 'My Shopping List', path: '/shop', hasProducts: products.length > 0, activeShop: true, productCSS: true});
 });
 
