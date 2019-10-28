0a615a3f95172f6c4b769c0cdd9d9f986e96b897
MarinaYsn <marina@bluelink.ca>
2019-10-28 19:15:15 -0400

    test

--- controllers/information.js
+++ controllers/information.js
@@ -3,6 +3,7 @@ exports.getAboutPageController = (req, res, next) =>{
     res.render('info/about', {docTitle: 'About Us', path: '/about', activeAbout: true, isLoggedIn: req.isLoggedIn })
 }
 
+
 exports.getDirectionController = (req, res, next) =>{
     res.render('info/direction', { docTitle: 'How To Find Us', path: '/direction', activeDirection: true, isLoggedIn: req.isLoggedIn  })
 }
