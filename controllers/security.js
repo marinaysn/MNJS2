d0ba173ed390a6ae29731ebc8214be0104abc72b
MarinaYsn <marinaysn@gmail.com>
2019-11-10 13:50:10 -0500

    added password vaidation

--- controllers/security.js
+++ controllers/security.js
@@ -72,7 +72,6 @@ exports.postSignUp = (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
     const errors = validationResult(req);
     
     if(!errors.isEmpty()){
@@ -83,7 +82,6 @@ exports.postSignUp = (req, res, next) => {
             errorMessage: errors.array()[0].msg
         });
     }
-
 
     User.findOne({ email: email })
         .then(userDoc => {
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const logIncontroller = require('../controllers/security');
 //const expValidator = require('express-validator/check');
-const {check} = require('express-validator');
+const { check,body } = require('express-validator');
 //LogIn
 routes.get('/login', logIncontroller.getLogInController);
 routes.post('/login', logIncontroller.postLogInController);
@@ -13,25 +13,38 @@ routes.post('/logout', logIncontroller.postLogOut);
 //signup
 routes.get('/signup', logIncontroller.getSignUp);
 routes.post(
-  '/signup', 
-  check('email')
-    .isEmail()
-    .withMessage('Please Enter Valid Email')
-    .custom((value, {req})=>{
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please Enter Valid Email')
+      .custom((value, { req }) => {
 
-      console.log('------------------')
-      console.log(value)
+        console.log('------------------')
+        console.log(value)
 
-      let regex = /tests.com/i;
-      let result = regex.test(value);
-      console.log(result);
+        let regex = /tests.com/i;
+        let result = regex.test(value);
+        console.log(result);
 
-      if(result){
+        if (result) {
 
-        throw new Error(`${value} Email Address is Reserved`)
-      }
-      return true;
-    }),
+          throw new Error(`${value} Email Address is Reserved`)
+        }
+        return true;
+      }),
+
+      body('password', 'Password should be at least 6 characters long and have only letters or numbers')
+      .isLength({min:5})
+      .isAlphanumeric(),
+
+      body('confirmPassword')
+      .custom((value, {req}) =>{
+        if(value !== req.body.password){
+          throw new Error('Passwords you entered do not match')
+        }
+      })
+  ],
   logIncontroller.postSignUp);
 
 //reset
