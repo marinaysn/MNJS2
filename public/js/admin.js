bf74583b5951861fafa19f138a7ebe33ffa0f417
MarinaYsn <marinaysn@gmail.com>
2019-11-18 20:57:01 -0500

    sending and handling background requests and manipulating DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,5 +3,20 @@ const deleteProduct =(btn) => {
 
     const prodID = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
-//btn.parentNode.querySelector('name=productId')
+    const productElement = btn.closest('article');
+
+    fetch('/admin/listOfProducts/' + prodID, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    }).then(result =>{
+        console.log(result)
+        return result.json();
+    }).then(data => {
+        console.log(data)
+       // productElement.remove();
+        productElement.parentNode.removeChild(productElement);
+    })
+    .catch(err => console.log(err));
 }
