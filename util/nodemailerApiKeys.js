a2421ecf7409473b90c402913d1d16be9fededa8
MarinaYsn <marina@bluelink.ca>
2019-12-14 11:45:41 -0500

    make it better

---
+++ util/nodemailerApiKeys.js
@@ -0,0 +1,4 @@
+//mongodb Atlas
+const nodemailerApiKey = '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'
+
+module.exports = nodemailerApiKey;
---
+++ util/stripeAPI.js
@@ -0,0 +1,4 @@
+//const stripeAPI = 'process.env/STRIPE_KEY'
+ const stripeAPI = 'sk_test_hoVsPuCKtdk6wrWoffO3DL6m00k6U7FQPk'
+
+module.exports = stripeAPI;
