66bf7ae4655c8e60de19159364985dfdb18335e7
MarinaYsn <marina@bluelink.ca>
2019-12-14 11:39:52 -0500

    make it better

---
+++ util/database.js
@@ -0,0 +1,12 @@
+const vEnv = require('../nodemon.json')
+//mongodb Atlas
+// const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@marnode-vxzld.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+const connectionString = 'mongodb+srv://marinaysn:enferced88@marnode-vxzld.mongodb.net/shop?retryWrites=true&w=majority'
+
+
+module.exports = connectionString;
+
+
+
+
